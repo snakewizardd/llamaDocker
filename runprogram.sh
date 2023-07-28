a2412530dffe2f634a65bf9965a283eb0997a5c7
snakewizardd <83378208+snakewizardd@users.noreply.github.com>
2023-07-27 20:51:07 -0400

    lacan

--- runprogram.sh
+++ runprogram.sh
@@ -4,9 +4,9 @@
 #./main -m ./models/7B/ggml-model-q4_0.bin --interactive-first --prompt "As a large language model I can offer my services to you as "
 ./main -m ./models/7B/ggml-model-q4_0.bin \
 -n 5000 \
---n-predict 2500 \
--c 2500 \
---ctx-size 2500 \
+--n-predict 5000 \
+-c 1800 \
+--ctx-size 1800 \
 --keep -1 \
 --temp .8 \
 --repeat-penalty 1.1 \
@@ -16,8 +16,8 @@
 --top-p .9 \
 --tfs 1.0 \
 --typical 1 \
---mirostat 2 \
---mirostat-lr .85 \
+--mirostat 0 \
+--mirostat-lr .1 \
 --mirostat-ent 5.0 \
 --mlock \
 --batch-size 512 \
