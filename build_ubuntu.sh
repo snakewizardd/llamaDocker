cb5365f69b7ff7920bca6094e7cf7a22663905b9
snakewizardd <83378208+snakewizardd@users.noreply.github.com>
2023-07-28 05:09:23 -0400

    clean

--- build_ubuntu.sh
+++ build_ubuntu.sh
@@ -1,4 +1,2 @@
 docker build -t ubuntu_llama_image -f DockerfileUbuntu.dockerfile .
-#docker run -it --name ubuntu_llama_container_server -p 8080:8080 --cpus=6 ubuntu_llama_image 
-#docker run -it --name ubuntu_llama_container_node -p 8080:8080 -p 3000:3000 --cpus=6 ubuntu_llama_image 
 docker-compose up
